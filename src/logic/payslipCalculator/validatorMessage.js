6f939a3b3a6e85915845cc7af0d84299776e32fe
hao <klaushaozhang@gmial.com>
2017-11-04 11:55:52 +1300

    clean ups

--- src/logic/payslipCalculator/validatorMessage.js
+++
@@ -1,4 +0,0 @@
-export const INPUT_NO_NULL = 'input argument cannot be null';
-export const INPUT_NO_EMPTY = 'input argument cannot be empty';
-export const INPUT_NO_UNDEFINED =
-  'input argument cannot be undefined';
